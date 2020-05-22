5f5b726817e5fcb4a799e9b9b05c44c0ae16e7eb
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-22 13:54:45 -0700

    add self.all() method to Project class.

--- lib/project.rb
+++ lib/project.rb
@@ -10,6 +10,14 @@ class Project
   end
 
   def self.all
+    returned_projects = DB.exec("SELECT * FROM projects")
+    projects = []
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
   end
 
   def save()
