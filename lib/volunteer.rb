4effe8b35e75df39720610924a8ba141b3094cd5
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-22 16:15:39 -0700

    write the remaining needed methods for Volunteer class.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -9,4 +9,28 @@ class Volunteer
     @project_id = attributes.fetch(:project_id)
     @id = attributes.fetch(:id)
   end
+
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
+  end
+
+  def self.find(project_id, id)
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
+    name = volunteer.fetch("name")
+    id = volunteer.fetch("id").to_i
+    project_id = volunteer.fetch("project_id").to_i
+    Volunteer.new({:name => name, :project_id => project_id, :id => id})
+  end
+
+  def ==(volunteer_to_compare)
+    self.name == volunteer_to_compare && self.id == volunteer_to_compare.id
+  end
 end
