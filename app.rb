a605e752c4bb4058b242f2d7859afdbef25e9ca9
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-31 12:23:24 -0700

    add get and post methods to list and add projects to a main webpage.

--- app.rb
+++ app.rb
@@ -3,4 +3,25 @@ require('sinatra/reloader')
 require('./lib/project')
 require('./lib/volunteer')
 require('pry')
+require('pg')
 also_reload('lib/**/*.rb')
+
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get('/') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+post('/projects') do
+  title = params[:title]
+  project = Project.new({:title => title, :id => nil})
+  project.save()
+  @projects = Project.all()
+  erb(:projects)
+end
