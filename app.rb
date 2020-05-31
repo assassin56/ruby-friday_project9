20aedec18ff167bb9fc9709435605c705b6350ff
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-31 12:51:45 -0700

    add routes for particular volunteers.

--- app.rb
+++ app.rb
@@ -58,3 +58,36 @@ delete('/projects/:id') do
   erb(:projects)
 end
 
+#  Methods pertaining to particular volunteers #
+
+get('/projects/:id/volunteers/:volunteer_id/edit') do
+  @project = Project.find(params[:id].to_i())
+  @volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  erb(:volunteer)
+end
+
+get('/projects/:id/volunteers/:volunteer_id/edit') do
+  @volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  erb(:volunteer)
+end
+
+post('/projects/:id/volunteers') do
+  @project = Project.find(params[:id].to_i())
+  @volunteer = Volunteer.new({:volunteer => (params[:new_volunteer]), :project_id => @project.id, :id => nil})
+  @volunteer.save()
+  erb(:project)
+end
+
+patch('/projects/:id/volunteers/:volunteer_id') do
+  @project = Project.find(params[:id].to_i())
+  @volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  @volunteer.update(params[:edit_volunteer], @project.id)
+  erb(:project)
+end
+
+delete('/projects/:id/volunteers/:volunteer_id') do
+  @volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  @volunteer.delete()
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
