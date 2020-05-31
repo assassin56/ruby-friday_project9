ac90ccaff963f52bb438dfbdc49ae25e1cb8d567
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-31 12:45:20 -0700

    add routes for particular projects to app.rb.

--- app.rb
+++ app.rb
@@ -8,11 +8,12 @@ also_reload('lib/**/*.rb')
 
 DB = PG.connect({:dbname => "volunteer_tracker"})
 
+# methods for the homepage #
+
 get('/') do
   @projects = Project.all
   erb(:projects)
 end
-
 get('/projects') do
   @projects = Project.all
   erb(:projects)
@@ -25,3 +26,35 @@ post('/projects') do
   @projects = Project.all()
   erb(:projects)
 end
+
+get('/projects/new') do
+  erb(:new_project)
+end
+
+# Methods pertaining to particular projects #
+
+get('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @projects = Project.all()
+  erb(:project)
+end
+
+get('/projects/:id/edit') do
+  @project = Project.find(params[:id].to_i())
+  erb(:edit_project)
+end
+
+patch('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.update(params[[:new_project])
+  @projects = Project.all()
+  erb(:projects)
+end
+
+delete('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.delete()
+  @projects = Project.all()
+  erb(:projects)
+end
+
